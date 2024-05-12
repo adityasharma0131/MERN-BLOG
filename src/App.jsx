a6790d4eb0607f480be5b1f77c8b0b766b7bfef2
adityasharma0131 <adityasharma0431@gmail.com>
2024-05-12 15:32:55 +0530

    Routing of pages to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+import Home from './pages/Home'
+import PostDetail from './pages/PostDetail'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+import Authors from './pages/Authors'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import CategoryPost from './pages/CategoryPost'
+import AuthorPosts from './pages/AutoPosts'
+import Dashboard from './pages/Dashboard'
+import Logout from './pages/Logout'
+import ErrorPage from './pages/ErrorPage'
+
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="posts/:id" element={<PostDetail/>}/>
+      <Route path="register" element={<Register/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="profile/:id" element={<UserProfile/>}/>
+      <Route path="authors" element={<Authors/>}/>
+      <Route path="create" element={<CreatePost/>}/>
+      <Route path="posts/:id/edit" element={<EditPost/>}/>
+      <Route path="posts/categories/:category" element={<CategoryPost/>}/>
+      <Route path="posts/users/:id" element={<AuthorPosts/>}/>
+      <Route path="myposts/:id" element={<Dashboard/>}/>
+      <Route path="logout" element={<Logout/>}/>
+      <Route path="*" element={<ErrorPage/>}/>
+
+      
+    </Routes>
+
+    <Footer />
+    </BrowserRouter>
+    
   )
 }
 
---
+++ src/pages/AutoPosts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AutoPosts = () => {
+  return (
+    <div>AutoPosts</div>
+  )
+}
+
+export default AutoPosts
