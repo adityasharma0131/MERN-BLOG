9a12182adf6bbe2b5e89ddf7c888ebda7a9bb3a3
adityasharma0131 <adityasharma0431@gmail.com>
2024-05-28 13:26:12 +0530

    skeleton for pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
