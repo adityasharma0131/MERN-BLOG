a8b7bffdf2d21dfc2755b3d1ffc4f22390fc23af
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-28 13:20:02 +0530

    navbar design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,24 @@
-import React from 'react'
-import './App.css'
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-
-import Home from './pages/Home'
-import PostDetail from './pages/PostDetail'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import UserProfile from './pages/UserProfile'
-import Authors from './pages/Authors'
-import CreatePost from './pages/CreatePost'
-import EditPost from './pages/EditPost'
-import CategoryPost from './pages/CategoryPost'
-import AuthorPosts from './pages/AutoPosts'
-import Dashboard from './pages/Dashboard'
-import Logout from './pages/Logout'
-import ErrorPage from './pages/ErrorPage'
-
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
 
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="posts/:id" element={<PostDetail/>}/>
-      <Route path="register" element={<Register/>}/>
-      <Route path="login" element={<Login/>}/>
-      <Route path="profile/:id" element={<UserProfile/>}/>
-      <Route path="authors" element={<Authors/>}/>
-      <Route path="create" element={<CreatePost/>}/>
-      <Route path="posts/:id/edit" element={<EditPost/>}/>
-      <Route path="posts/categories/:category" element={<CategoryPost/>}/>
-      <Route path="posts/users/:id" element={<AuthorPosts/>}/>
-      <Route path="myposts/:id" element={<Dashboard/>}/>
-      <Route path="logout" element={<Logout/>}/>
-      <Route path="*" element={<ErrorPage/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
 
-      
-    </Routes>
-
-    <Footer />
+      <Footer />
     </BrowserRouter>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+      <h1 className="text-xl font-extrabold border-black border-2 p-1">
+        <Link to="/">MERN-BLOG</Link>
+      </h1>
+      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+        <h3>Login</h3>
+        <h3>Register</h3>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
