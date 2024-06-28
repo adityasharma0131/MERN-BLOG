2bf762e10ef981b2354d8f992fc9a6571202dbe7
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-28 11:11:44 +0530

    tailwind css , axios, react-icons installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 
 import Home from './pages/Home'
